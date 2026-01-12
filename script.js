4054548f49dc6e1f11459aaae9e3bdedc0e910bc
AKDavis95 <112223134+AKDavis95@users.noreply.github.com>
2026-01-11 22:42:15 -0500

    2nd commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const navOpen = document.querySelector('.fa-bars');
+const navClose = document.querySelector('.fa-x');
+const navMenu = document.querySelector('nav');
+
+// open nav with click of bars
+const openNavMenu = () => {
+  navMenu.style.right = '0px';
+};
+navOpen.addEventListener('click', openNavMenu);
+
+// close nav with x mark
+const closeNavMenu = () => {
+  navMenu.style.right = '-355px';
+};
+navClose.addEventListener('click', closeNavMenu);
+
+// click on site to exit nav menu
+document.addEventListener('click', (e) => {
+  if (!navMenu.contains(e.target) && e.target !== navOpen) {
+    navMenu.style.right = '-355px';
+  }
+});
+
+// lightbox code webdevsimplified not working
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+const lightboxImages = document.querySelectorAll('.lightbox');
+lightboxImages.forEach((image) => {
+  image.addEventListener('click', (e) => {
+    lightbox.classList.add('active');
+    const img = document.createElement('img');
+    img.src = image.src;
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    lightbox.appendChild(img);
+  });
+});
+
+lightbox.addEventListener('click', (e) => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove('active');
+});
